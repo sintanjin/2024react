a932c242a82b3354b26aad5c86b081766ed919c9
Taeyeon <z01230z@handong.ac.kr>
2024-11-09 10:44:33 +0900

    4-2

---
+++ newproject/src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          오픈소스 스튜디오 22100215 김태연
+        </p>
+        <a
+          className="App-link"
+          href="https://github.com/sintanjin/2024-4-2"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Github address
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
